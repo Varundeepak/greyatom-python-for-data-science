8480f3947aa895d53a1537d45e352d7b07bd3646
Varundeepak <49053269+Varundeepak@users.noreply.github.com>
2020-05-08 23:22:36 +0530

    Update code.py

--- GrayAtom-Python-For-Data-Science/code.py
+++ GrayAtom-Python-For-Data-Science/code.py
@@ -1,73 +1,78 @@
 # --------------
 # Code starts here
+
+# Create the lists 
 class_1=["Geoffrey Hinton","Andrew Ng","Sebastian Raschka","Yoshua Bengio"]
 class_2=["Hilary Mason","Carla Gentry","Corinna Cortes"]
-# Create the lists 
-new_class=class_1+class_2
-#print(new_class)
+
 # Concatenate both the strings
-new_class.append("Peter Warden")
-#print(new_class)
-new_class.remove("Carla Gentry")
+new_class=class_1+class_2
 print(new_class)
 
 # Append the list
+new_class.append("Peter Warden")
 
 # Print updated list
-courses={"Math":65,"English":70,"History":80,"French":70,"Science":60}
-total=sum(courses.values())
-percentage=(total/500)*100
-print(total)
-print(percentage)
+print(new_class)
+
 
 # Remove the element from the list
+new_class.remove("Carla Gentry")
+
 
 # Print the list
+print(new_class)
+
+
+
+# Create the Dictionary
+courses={"Math":65,"English":70,"History":80,"French":70,"Science":60}
+
+
+# Store the all the subject in one variable `Total`
+total=sum(courses.values())
+
+# Print the total
+print(total)
+
+
+# Insert percentage formula
+percentage=(total/500)*100
+
+
+
+# Print the percentage
+print(percentage)
+
+
+
+# Create the Dictionary
 mathematics={"Geoffrey Hinton":78,"Andrew Ng":95,"Sebastian Raschka":65,"Yoshua Benjio":50,"Hilary Mason":70,"Corinna Cortes":66,"Peter Warden":75}
+
+
+
+# Find the topper in the dictionary
 topper=max(mathematics,key=mathematics.get)
 print(topper)
 
 
-# Create the Dictionary
+#Create variables first_name and last_name
 first_name,last_name=topper.split()[0],topper.split()[1]
+
+
+
+#Print first_name and last_name
 print(first_name)
 print(last_name)
+
+
+#Concatenate last_name and first_name to form certificate name (last name comes first, and then first name)
 full_name=last_name+" "+first_name
+
+
+#Convert the certificate name to upper case
 certificate_name=full_name.upper()
+
+#Print the certificate name
 print(certificate_name)
-
-# Slice the dict and stores  the all subjects marks in variable
-
-
-# Store the all the subject in one variable `Total`
-
-# Print the total
-
-# Insert percentage formula
-
-# Print the percentage
-
-
-
-
-# Create the Dictionary
- 
-
-
-
-# Given string
-
-
-# Create variable first_name 
-
-# Create variable Last_name and store last two element in the list
-
-# Concatenate the string
-
-# print the full_name
-
-# print the name in upper case
-
-# Code ends here
-
 
